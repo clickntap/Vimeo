92ab43d42b8676852570c7f97668cd6e5c7657c0
tmendici <info@clickntap.com>
2014-10-01 21:38:38 +0200

    added params in video upload (work in progress)

--- VimeoApi/src/main/java/com/clickntap/vimeo/Vimeo.java
+++ VimeoApi/src/main/java/com/clickntap/vimeo/Vimeo.java
@@ -1,17 +1,18 @@
 package com.clickntap.vimeo;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Map;
 
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.JSONException;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
 public class Vimeo {
@@ -23,20 +24,27 @@ public class Vimeo {
 	}
 
 	public JSONObject getVideos() throws Exception {
-		return apiRequest("/me/videos", HttpGet.METHOD_NAME);
+		return apiRequest("/me/videos", HttpGet.METHOD_NAME, null);
 	}
 
-	public JSONObject uploadVideo() throws Exception {
-		return apiRequest("/me/videos", HttpPost.METHOD_NAME);
+	public JSONObject uploadVideo(Map<String, String> params) throws Exception {
+		return apiRequest("/me/videos", HttpPost.METHOD_NAME, params);
 	}
 
-	private JSONObject apiRequest(String endpoint, String methodName) throws IOException, ClientProtocolException, UnsupportedEncodingException, JSONException {
+	private JSONObject apiRequest(String endpoint, String methodName, Map<String, String> params) throws Exception {
 		CloseableHttpClient client = HttpClientBuilder.create().build();
 		HttpRequestBase request = null;
 		if (methodName.equals(HttpGet.METHOD_NAME)) {
 			request = new HttpGet(new StringBuffer(VIMEO_SERVER).append(endpoint).toString());
 		} else if (methodName.equals(HttpPost.METHOD_NAME)) {
 			request = new HttpPost(new StringBuffer(VIMEO_SERVER).append(endpoint).toString());
+		}
+		if (params != null && request instanceof HttpPost) {
+			ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+			for (String key : params.keySet()) {
+				postParameters.add(new BasicNameValuePair(key, params.get(key)));
+			}
+			((HttpPost) request).setEntity(new UrlEncodedFormEntity(postParameters));
 		}
 		request.addHeader("Authorization", new StringBuffer("bearer ").append(bearerToken).toString());
 		request.addHeader("Accept", "application/vnd.vimeo.*+json; version=3.2");
---
+++ VimeoApi/src/main/java/com/clickntap/vimeo/VimeoParams.java
@@ -0,0 +1,7 @@
+package com.clickntap.vimeo;
+
+import org.apache.http.HttpEntity;
+
+public interface VimeoParams {
+	public HttpEntity getEntity() throws Exception;
+}
